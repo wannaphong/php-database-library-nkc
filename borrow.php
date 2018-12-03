010eef6171af769dc48a4ef54171dd091cd17dd1
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2018-12-04 01:02:02 +0700

    save code

---
+++ borrow.php
@@ -0,0 +1,48 @@
+<?php
+/*
+หน้านี้เป็นหน้าสำหรับเพิ่มรายการที่ยืม หรือ ลบรายการที่ยืม โดยสามารถยกเลิกการยืมได้
+*/
+require_once("config.php");
+
+/*
+แสดงรายการหนังสือที่ยืม ในการยืมแต่ละครั้ง
+
+- เลือกหนังสือที่จะยิมเพิ่ม
+- หากต้องการยกเลิกการยืม สามารถกดยกเลิกได้
+*/
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>ทดสอบ</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+    <header><?php echo $name_web;?></header>
+    <?php include('nav.php');?>
+    <main>
+      <article>
+        <h1>รายการยืมหนังสือ</h1>
+        <?php
+        ?>
+        <form action="./save_add_student.php"  method="POST">
+        <br>ชื่อ : <input type="text" name="name"><br>
+        ที่อยู่ : <input type="text" name="address"><br>
+        ปีเดือนวันเกิด : <input type="date" name="dob"><br>
+        เพศ : <input type="text" name="gender"><br>
+        เบอร์ : <input type="tel" name="phone"><br>
+        <input type="submit" value="Submit">
+        </form>
+        <p>Content</p>
+    </article>
+	  <aside>
+	    <p>More information</p>
+	  </aside>
+    </main>
+    <footer>Copyright ทดสอบ</footer>
+</body>
+</html>
