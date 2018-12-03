7e8148f0cb7761771ad101d20f6a80ee617a1826
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2018-12-03 16:35:19 +0700

    add librarian

---
+++ add_librarian.php
@@ -0,0 +1,35 @@
+<?php
+require_once("config.php");
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>ทดสอบ</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+    <header><?php echo $name_web;?></header>
+    <?php include('nav.php');?>
+    <main>
+      <article>
+        <h1>เพิ่มข้อมูล</h1>
+        <form action="./save_add_librarian.php"  method="POST">
+        <br>ชื่อ : <input type="text" name="name"><br>
+        ที่อยู่ : <input type="text" name="address"><br>
+        IDcard : <input type="text" name="idcard"><br>
+        เพศ : <input type="text" name="gender"><br>
+        <input type="submit" value="Submit">
+        </form>
+        <p>Content</p>
+    </article>
+	  <aside>
+	    <p>More information</p>
+	  </aside>
+    </main>
+    <footer>Copyright ทดสอบ</footer>
+</body>
+</html>
