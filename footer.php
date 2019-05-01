843df4266bf5f3bf031434ed64f0b42382ad8954
Wannaphong <wannaphong@kkumail.com>
2019-05-02 06:02:20 +0700

    add scrolls.js

--- footer.php
+++ footer.php
@@ -11,4 +11,5 @@
   <!--  Scripts-->
   <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+  <script src="js/scrolls.js"></script>
   <script src="js/init.js"></script>
--- js/init.js
+++ js/init.js
@@ -5,5 +5,6 @@
     $('select').formSelect();
      $('.slider').slider();
     $('input#input_text, textarea#textarea2').characterCounter();
+    scroller.init();
   }); // end of document ready
 })(jQuery); // end of jQuery name space
